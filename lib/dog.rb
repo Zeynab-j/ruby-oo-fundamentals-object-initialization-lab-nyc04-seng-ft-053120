82ac95ff1e6cf76de80b10db75984f19a0d3a4da
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-23 19:50:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,2 @@
 class Dog
-  def initialize(breed)
-    @breed = breed
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
-end
-lassie = Dog.new("Mutt")
-
-lassie.breed
+  @name = name 
